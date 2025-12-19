cf3dfb5b7ca0c6316ef836d529cca2403ede0c2c
inbento <mikov.ns.04@gmail.com>
2025-12-19 21:39:12 +0300

    add Lab1

---
+++ Lab1/WebApi/Program.cs
@@ -0,0 +1,48 @@
+// создается билдер веб приложения
+
+using Dapper;
+using FluentValidation;
+using System.Threading.Tasks;
+using WebApi.BLL.Services;
+using WebApi.DAL;
+using WebApi.DAL.Interfaces;
+using WebApi.DAL.Repositories;
+using WebApi.Validators;
+
+var builder = WebApplication.CreateBuilder(args);
+
+DefaultTypeMap.MatchNamesWithUnderscores = true;
+builder.Services.AddScoped<UnitOfWork>();
+
+builder.Services.Configure<DbSettings>(builder.Configuration.GetSection(nameof(DbSettings)));
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+builder.Services.AddScoped<OrderService>();
+builder.Services.AddValidatorsFromAssemblyContaining(typeof(Program));
+builder.Services.AddScoped<ValidatorFactory>();
+
+// зависимость, которая автоматически подхватывает все контроллеры в проекте
+builder.Services.AddControllers();
+// добавляем swagger
+builder.Services.AddSwaggerGen();
+
+// собираем билдер в приложение
+var app = builder.Build();
+
+// добавляем 2 миддлвари для обработки запросов в сваггер
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// добавляем миддлварю для роутинга в нужный контроллер
+app.MapControllers();
+
+// ждем, чтобы база данных успела запуститься
+await Task.Delay(10000);
+
+// вместо *** должен быть путь к проекту Migrations
+// по сути в этот момент будет происходить накатка миграций на базу
+Migrations.Program.Main([]);
+
+// запускам приложение
+app.Run();
